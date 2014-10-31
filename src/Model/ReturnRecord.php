d951e05d2da74674afeba5563b2be54c3f320268
Jing Li <j.li@linkorb.com>
2014-10-31 17:20:30 +0800

    Return record.

--- src/Model/ReturnRecord.php
+++ src/Model/ReturnRecord.php
@@ -3,6 +3,7 @@
 namespace ReturnManager\Model;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use ReturnManager\Model\ReturnLine;
 
 class ReturnRecord implements ReturnRecordInterface
 {
@@ -23,5 +24,61 @@ class ReturnRecord implements ReturnRecordInterface
     public function getKey()
     {
         return $this->key;
+    }
+
+    public function addLine(ReturnLine $line)
+    {
+        $this->lines[] = $line;
+    }
+
+    private $totalPrice;
+
+    public function getTotalPrice()
+    {
+        return $this->totalPrice;
+    }
+
+    public function setTotalPrice()
+    {
+        $totalPrice = 0;
+        foreach ($this->lines as $line) {
+            $totalPrice += $line->getTotalPrice();
+        }
+        $this->totalPrice = $totalPrice;
+        return $this;
+    }
+
+    private $vatPrice;
+
+    public function setVatPrice()
+    {
+        $vatPrice = 0;
+        foreach ($this->lines as $line) {
+            $vatPrice += $line->getVatPrice();
+        }
+        $this->vatPrice = $vatPrice;
+        return $this;
+    }
+
+    public function getVatPrice()
+    {
+        return $this->vatPrice;
+    }
+
+    private $orderPrice;
+
+    public function getOrderPrice()
+    {
+        return $this->orderPrice;
+    }
+
+    public function setOrderPrice()
+    {
+        $orderPrice = 0;
+        foreach ($this->lines as $line) {
+            $orderPrice += $line->getUnitPriceTotal();
+        }
+        $this->orderPrice = $orderPrice;
+        return $this;
     }
 }
